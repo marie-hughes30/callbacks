b7af05f395853e037ebda1db0fd6ec3ad9f5d8f3
marie hughes <mariehughes@maries-MacBook-Pro.local>
2016-10-04 09:47:28 +0100

    Feature a comit

--- callbackexample.js
+++ callbackexample.js
@@ -14,6 +14,7 @@
 // console.log("Synchronous read: " + data.toString());
 //
 // console.log("Program Ended")
+
 downloadPhoto('https://static.pexels.com/photos/40815/youth-active-jump-happy-40815-medium.jpeg', handlePhoto)
 
 function handlePhoto (error, photo) {
@@ -21,9 +22,18 @@ function handlePhoto (error, photo) {
   else console.log('Download finished', photo)
 }
 
+// downloadPhoto('https://static.pexels.com/photos/40815/youth-active-jump-happy-40815-medium.jpeg', handlePhoto)
+//
+// function handlePhoto (error, photo) {
+//   if (error) console.error('Download error!', error)
+//   else console.log('Download finished', photo)
+// }
+
+
 console.log('Download started')
 console.log('Traning in git')
 console.log("Last test")
 console.log("las test")
 console.log("Have now switched to feature b")
-console.log("Add another line to featureb") 
+console.log("Add another line to featureb")
+console.log("Feature a")
